5dee5a7c79660141ffb06cbc0cc43902143cd5e2
agussanguesa32 <agussanguesa@gmail.com>
2023-05-20 14:26:21 -0300

    agregados estilos a la tabla y sorting

--- views/leaderboard/script.js
+++ views/leaderboard/script.js
@@ -1,55 +1,17 @@
 $(document).ready(function() {
-    var table = $('#dtBasicExample').DataTable({
-      "columnDefs": [
-        { "type": "numeric", "targets": [2, 3] },
-        { "type": "string", "targets": [1] }
-      ],
-      "order": [[0, 'asc']], // Orden ascendente inicial en la columna 0
-      "dom": 'lfrtip',
-      "buttons": [
-        'colvis',
-        { extend: 'csv', text: 'Exportar CSV' },
-        { extend: 'excel', text: 'Exportar Excel' },
-        { extend: 'pdf', text: 'Exportar PDF' }
-      ],
-      "initComplete": function(settings, json) {
-        updateArrows();
-      }
-    });
-  
-    function updateArrows() {
-      $('#dtBasicExample thead th').each(function(index) {
-        var column = $(this);
-        var arrow = column.find('.sort-arrow');
-        arrow.removeClass('arrow-up arrow-down');
-        if (table.order()[0][0] === index) {
-          if (table.order()[0][1] === 'asc') {
-            arrow.addClass('arrow-up');
-          } else {
-            arrow.addClass('arrow-down');
+  $('#example').DataTable( {
+      responsive: {
+          details: {
+              display: $.fn.dataTable.Responsive.display.modal( {
+                  header: function ( row ) {
+                      var data = row.data();
+                      return 'Details for '+data[0]+' '+data[1];
+                  }
+              } ),
+              renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+                  tableClass: 'table'
+              } )
           }
-        } else {
-          arrow.removeClass('arrow-up arrow-down');
-        }
-      });
-    }
-  
-    $('#dtBasicExample thead th').each(function(index) {
-      if (index === 0 || index === 2 || index === 3 || index === 4) {
-        var column = $(this);
-        var arrow = column.find('.sort-arrow');
-        arrow.on('click', function() {
-          var currentOrder = table.order()[0];
-          if (currentOrder[0] === index && currentOrder[1] === 'asc') {
-            table.order([index, 'desc']).draw();
-          } else {
-            table.order([index, 'asc']).draw();
-          }
-          updateArrows();
-        });
       }
-    });
-  
-    updateArrows();
-  });
-  
+  } );
+} );
