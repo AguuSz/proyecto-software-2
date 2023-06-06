201a22f831bcc215ccd6bed0ab20a02dfae72287
AguuSz <agus.sepu2000@hotmail.com>
2023-06-06 09:28:12 -0300

    lobbies are now interacting with DB

--- views/matchmaking/script.js
+++ views/matchmaking/script.js
@@ -2,13 +2,15 @@ const toastTrigger = document.getElementById("createLobbyBtn");
 const toastModalSuccess = document.getElementById("successToast");
 const toastModalError = document.getElementById("errorToast");
 
+const toastSucess = bootstrap.Toast.getOrCreateInstance(toastModalSuccess);
+const toastError = bootstrap.Toast.getOrCreateInstance(toastModalError);
+const myModal = new bootstrap.Modal(document.getElementById("newLobbyModal"));
+
 if (toastTrigger) {
-	const toastSucess = bootstrap.Toast.getOrCreateInstance(toastModalSuccess);
-	const toastError = bootstrap.Toast.getOrCreateInstance(toastModalError);
-	const myModal = new bootstrap.Modal(document.getElementById("newLobbyModal"));
 	toastTrigger.addEventListener("click", () => {
+		event.preventDefault();
 		if (isNewLobbyFormValid()) {
-			appendLobby("SharkyMid");
+			createLobby();
 			myModal.hide();
 			toastSucess.show();
 		} else {
@@ -19,31 +21,44 @@ if (toastTrigger) {
 
 function isNewLobbyFormValid() {
 	var platform = document.forms["newLobbyModalForm"]["platform"].value;
-	var rank = document.forms["newLobbyModalForm"]["rank"].value;
-	var division = document.forms["newLobbyModalForm"]["division"].value;
 	var region = document.forms["newLobbyModalForm"]["region"].value;
-	var mmr = document.forms["newLobbyModalForm"]["mmr"].value;
 
-	return (
-		platform != "Choose..." &&
-		rank != "Choose..." &&
-		division != "Choose..." &&
-		region != "Choose..." &&
-		mmr != "Choose..."
-	);
+	return platform != "Choose..." && region != "Choose...";
 }
 
-function appendLobby(username) {
-	var platform = document.forms["newLobbyModalForm"]["platform"].value;
-	var rank = document.forms["newLobbyModalForm"]["rank"].value;
-	var division = document.forms["newLobbyModalForm"]["division"].value;
+// Funcion que se llama cuando el usuario clickea en "Create new Lobby" en el Modal.
+async function createLobby() {
+	// Obtenemos los valores del form
+	var platform =
+		document.forms["newLobbyModalForm"]["platform"].value.toUpperCase();
 	var region =
 		document.forms["newLobbyModalForm"]["region"].value.toUpperCase();
-	var mmr = document.forms["newLobbyModalForm"]["mmr"].value;
 
-	// Hacemos esto para que se ponga la primer letra en mayúscula
-	rank = rank.charAt(0).toUpperCase() + rank.slice(1);
+	var user = await getUserById(getFromCookies("userId"));
+	let newLobby = {
+		userId: user.id,
+		platform: platform,
+		region: region,
+		isFull: false,
+		blueTeam: [],
+		redTeam: [],
+	};
 
+	try {
+		await storeLobby(newLobby);
+		// Para poder agregar el lobby es necesario que tenga informacion del usuario (para saber rank y division), pero esta info no esta presente en el lobby que se guarda en DB, por eso se setea
+		let tempLobby = newLobby;
+		tempLobby.user = user;
+		appendLobby(tempLobby);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function appendLobby(lobby) {
+	let user = lobby.user;
+
+	// Obtiene donde insertar el div
 	const lobbys = document.getElementById("lobbys");
 
 	// Crear el elemento div con la clase "col-2"
@@ -57,7 +72,7 @@ function appendLobby(username) {
 	// Crear el elemento div con la clase "card-header" y texto "AguuSz's lobby"
 	const divCardHeader = document.createElement("div");
 	divCardHeader.classList.add("card-header", "pe-1");
-	divCardHeader.textContent = username + "'s lobby";
+	divCardHeader.textContent = user.nickname + "'s lobby";
 	const closeButton = document.createElement("button");
 	closeButton.setAttribute("type", "button");
 	closeButton.classList.add("btn-close", "float-end", "show-if-admin");
@@ -68,22 +83,22 @@ function appendLobby(username) {
 	divCardBody.classList.add("card-body");
 
 	// Crear el elemento h6 con la clase "card-subtitle mb-3 text-body-secondary" y texto "SAM"
-	const h6CardSubtitleSAM = document.createElement("h6");
-	h6CardSubtitleSAM.classList.add(
+	const h6CardSubtitleRegion = document.createElement("h6");
+	h6CardSubtitleRegion.classList.add(
 		"card-subtitle",
 		"mb-3",
 		"text-body-secondary"
 	);
-	h6CardSubtitleSAM.textContent = region;
+	h6CardSubtitleRegion.textContent = lobby.region;
 
-	// Crear el elemento h6 con la clase "card-subtitle mb-3 text-body-secondary" y texto "Diamond I"
-	const h6CardSubtitleDiamondI = document.createElement("h6");
-	h6CardSubtitleDiamondI.classList.add(
+	// Crear el elemento h6 con la clase "card-subtitle mb-3 text-body-secondary" para el Rank + Division
+	const h6CardSubtitleRank = document.createElement("h6");
+	h6CardSubtitleRank.classList.add(
 		"card-subtitle",
 		"mb-3",
 		"text-body-secondary"
 	);
-	h6CardSubtitleDiamondI.textContent = rank + " " + division;
+	h6CardSubtitleRank.textContent = user.rank + " " + user.division;
 
 	// Crear el elemento img con el atributo src y clase "rounded" para las tres imágenes
 	const imgProfilePic = document.createElement("img");
@@ -97,8 +112,16 @@ function appendLobby(username) {
 	h5VS.classList.add("my-3");
 	h5VS.textContent = "VS";
 
-	// Crear el botón "Join lobby!" con la clase "btn btn-primary mt-3" y el elemento a con el atributo href
+	// Crear el elemento h6 con la clase "card-subtitle mb-3 text-body-secondary" para mostrar la plataforma
+	const h6CardSubtitlePlatform = document.createElement("h6");
+	h6CardSubtitlePlatform.classList.add(
+		"card-subtitle",
+		"my-3",
+		"text-body-secondary"
+	);
+	h6CardSubtitlePlatform.textContent = lobby.platform;
 
+	// Crear el botón "Join lobby!" con la clase "btn btn-primary mt-3" y el elemento a con el atributo href
 	const aJoinLobby = document.createElement("a");
 	aJoinLobby.href = "#";
 	aJoinLobby.classList.add("btn", "btn-primary", "mt-3");
@@ -109,14 +132,14 @@ function appendLobby(username) {
 	divRow.classList.add("row", "mx-auto");
 	divRow.appendChild(aJoinLobby);
 
-	// Crear el elemento div con la clase "card-footer text-body-secondary" y texto "5 minutes ago"
+	// Crear el elemento div con la clase "card-footer text-body-secondary"
 	const divCardFooter = document.createElement("div");
 	divCardFooter.classList.add("card-footer", "text-body-secondary");
 	divCardFooter.textContent = "1 second ago";
 
 	// Agregar todos los elementos creados al elemento padre correspondiente
-	divCardBody.appendChild(h6CardSubtitleSAM);
-	divCardBody.appendChild(h6CardSubtitleDiamondI);
+	divCardBody.appendChild(h6CardSubtitleRegion);
+	divCardBody.appendChild(h6CardSubtitleRank);
 	divCardBody.appendChild(imgProfilePic.cloneNode());
 	divCardBody.appendChild(imgProfilePic.cloneNode());
 	divCardBody.appendChild(imgProfilePic.cloneNode());
@@ -124,6 +147,7 @@ function appendLobby(username) {
 	divCardBody.appendChild(imgProfilePic.cloneNode());
 	divCardBody.appendChild(imgProfilePic.cloneNode());
 	divCardBody.appendChild(imgProfilePic.cloneNode());
+	divCardBody.appendChild(h6CardSubtitlePlatform);
 	divCardBody.appendChild(divRow);
 
 	divCard.appendChild(divCardHeader);
@@ -133,4 +157,68 @@ function appendLobby(username) {
 	divCol2.appendChild(divCard);
 
 	lobbys.appendChild(divCol2);
+}
+
+// Funcion que obtiene los lobbies apenas se carga la pagina
+window.onload = () => {
+	getLobbies();
+};
+
+function getLobbies() {
+	fetch(`http://localhost:3000/lobbies?_expand=user`)
+		.then((response) => response.json())
+		.then((responseLobbies) => {
+			responseLobbies.forEach((lobby) => {
+				appendLobby(lobby);
+			});
+		})
+		.catch((error) => {
+			console.log("Error while trying to fetch data:", error);
+		});
+}
+
+function getUserById(id) {
+	return fetch(`http://localhost:3000/users/${id}`)
+		.then((response) => response.json())
+		.then((user) => {
+			if (user) {
+				return user;
+			} else {
+				return null;
+			}
+		})
+		.catch((error) => {
+			console.log("Error while trying to fetch data:", error);
+		});
+}
+
+function storeLobby(lobby) {
+	const url = "http://localhost:3000/lobbies";
+
+	return fetch(url, {
+		method: "POST",
+		body: JSON.stringify(lobby),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((result) => {
+			return result;
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
+}
+
+function storeInCookies(key, value) {
+	localStorage.setItem(key, value);
+}
+
+function getFromCookies(key) {
+	return localStorage.getItem(key);
+}
+
+function deleteFromCookies(key) {
+	localStorage.removeItem(key);
 }
